befe085d96003e954069bf55b2fda74b3523c132
krlld <kirilldikun25@gmail.com>
2024-03-05 21:04:10 +0300

    first commit

---
+++ src/main/java/by/kirilldikun/cvservice/CvServiceApplication.java
@@ -0,0 +1,13 @@
+package by.kirilldikun.cvservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvServiceApplication.class, args);
+    }
+
+}
