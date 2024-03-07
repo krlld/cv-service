6d3d5d76e2044457db0c857e12d93bd3d727959c
krlld <kirilldikun25@gmail.com>
2024-03-07 12:39:19 +0300

    feat: add migration with directions data

---
+++ src/main/resources/db/migration/V2__add_directions_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO directions (name, description)
+VALUES ('Web Development', 'Designing and building websites and web applications');
+INSERT INTO directions (name, description)
+VALUES ('Mobile App Development', 'Creating applications for mobile devices like smartphones and tablets');
+INSERT INTO directions (name, description)
+VALUES ('Data Science',
+        'Analyzing and interpreting large sets of data to extract insights and make informed decisions');
+INSERT INTO directions (name, description)
+VALUES ('Cybersecurity', 'Protecting computer systems and networks from unauthorized access and attacks');
+INSERT INTO directions (name, description)
+VALUES ('Cloud Computing', 'Using remote servers to store, manage, and process data and applications');
+INSERT INTO directions (name, description)
+VALUES ('Artificial Intelligence',
+        'Developing intelligent machines and systems that can perform tasks requiring human intelligence');
+INSERT INTO directions (name, description)
+VALUES ('Blockchain Development', 'Building decentralized and secure systems for recording and verifying transactions');
+INSERT INTO directions (name, description)
+VALUES ('UI/UX Design', 'Creating intuitive and visually appealing user interfaces and user experiences');
+INSERT INTO directions (name, description)
+VALUES ('IT Project Management',
+        'Planning, organizing, and overseeing IT projects to ensure successful implementation');
