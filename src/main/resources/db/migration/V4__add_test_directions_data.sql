8c5cbcd3dc5c24fcf9219843883126b98f18efe1
krlld <kirilldikun25@gmail.com>
2024-03-07 12:39:53 +0300

    feat: add migration with test directions data

---
+++ src/main/resources/db/migration/V4__add_test_directions_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (1, 1);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (2, 1);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (3, 2);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (4, 3);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (5, 4);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (6, 5);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (7, 6);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (8, 7);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (9, 8);
+INSERT INTO test_directions (test_id, direction_id)
+VALUES (10, 9);
