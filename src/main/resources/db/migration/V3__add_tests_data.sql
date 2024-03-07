6e3d51d03ecbb290165c29d4f2b2aa797a18f470
krlld <kirilldikun25@gmail.com>
2024-03-07 12:39:34 +0300

    feat: add migration with tests data

---
+++ src/main/resources/db/migration/V3__add_tests_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO tests (name, description)
+VALUES ('Math Test', 'A test to evaluate mathematical knowledge and problem-solving skills');
+INSERT INTO tests (name, description)
+VALUES ('Programming Test', 'A test to assess programming skills and logical thinking');
+INSERT INTO tests (name, description)
+VALUES ('English Language Test', 'A test to evaluate proficiency in the English language');
+INSERT INTO tests (name, description)
+VALUES ('Data Analysis Test', 'A test to assess skills in analyzing and interpreting data');
+INSERT INTO tests (name, description)
+VALUES ('Logical Reasoning Test', 'A test to evaluate logical reasoning and critical thinking abilities');
+INSERT INTO tests (name, description)
+VALUES ('Personality Assessment', 'A test to assess personality traits, strengths, and weaknesses');
+INSERT INTO tests (name, description)
+VALUES ('Project Management Test', 'A test to evaluate knowledge and skills in project management');
+INSERT INTO tests (name, description)
+VALUES ('Problem-Solving Test', 'A test to assess problem-solving abilities and decision-making skills');
+INSERT INTO tests (name, description)
+VALUES ('Communication Skills Test', 'A test to evaluate verbal and written communication skills');
+INSERT INTO tests (name, description)
+VALUES ('Analytical Skills Test', 'A test to assess analytical thinking and problem-solving capabilities');
